53371e72fc78e893ffd1acb6b8f57b23859c50c8
Alin Ghiurca <ghiurcaalin@gmail.com>
2018-10-25 19:07:31 +0300

    prepopulate users and questions tables using model facory adn seed

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Question;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       factory(App\User::class,3)->create()->each(function($u)
+      {
+            $u->questions()
+            ->saveMany(
+                factory(App\Question::class,rand(1,5))->make()
+            );
+       });
+    }
+}
